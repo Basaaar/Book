ea36f3c3f66a6a2e9c2a56da6a7332b7ee8c22e1
sbasar <senolbasaar@gmail.com>
2024-04-29 21:37:29 +0300

    --Arayüzde güncellemeler yapıldı.

--- Bulky/BulkyWeb/Data/ApplicationDbContext.cs
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace BulkyWeb.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c305ca60545c209594746e34c44bbec843dc9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e169e000d0200df3152a0aea72cd6c75312553")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
