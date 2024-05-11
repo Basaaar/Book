05931578e01fbcd101829a724402d59c67e7b8c2
sbasar <senolbasaar@gmail.com>
2024-05-11 17:44:59 +0300

    --Arayüz güncellemesi yapıldı.

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5764851e8bde1d613cab91ce24db4f77000e75b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53a417c18bfd8cd036877025daff7554a9299e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
