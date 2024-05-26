d5bd84475063680358aa947d7b28314cc455efd7
sbasar <senolbasaar@gmail.com>
2024-05-26 22:08:05 +0300

    --Summary eklendi.

--- Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -9,7 +9,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
     [Authorize]
-    public class CartController:Controller
+    public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM ShoppingCartVM { get; set; }
@@ -21,17 +21,55 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
         {
             var claimsIdentiy = (ClaimsIdentity)User.Identity;//Default property
             var userID = claimsIdentiy.FindFirst(ClaimTypes.NameIdentifier).Value;
-            ShoppingCartVM = new ()
+            ShoppingCartVM = new()
             {
-                ShoppingCartList = _unitOfWork.ShoopingCart.GetAll(u => u.ApplicationUserId == userID,includeProperties:"Product")
+                ShoppingCartList = _unitOfWork.ShoopingCart.GetAll(u => u.ApplicationUserId == userID, includeProperties: "Product")
             };
-            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
-                cart.Price=GetPriceBasedOnQuantity(cart);
+                cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
             }
             return View(ShoppingCartVM);
         }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+        public IActionResult Plus(int carId)
+        {
+            var cartFromDB = _unitOfWork.ShoopingCart.Get(u => u.Id == carId);
+            cartFromDB.Count += 1;
+            _unitOfWork.ShoopingCart.Update(cartFromDB);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Minus(int carId)
+        {
+            var cartFromDB = _unitOfWork.ShoopingCart.Get(u => u.Id == carId);
+            if (cartFromDB.Count <= 1)
+            {
+                _unitOfWork.ShoopingCart.Remove(cartFromDB);
+            }
+            else
+            {
+                cartFromDB.Count -= 1;
+                _unitOfWork.ShoopingCart.Update(cartFromDB);
+            }
+
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Remove(int carId)
+        {
+            var cartFromDB = _unitOfWork.ShoopingCart.Get(u => u.Id == carId);
+            _unitOfWork.ShoopingCart.Remove(cartFromDB);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
@@ -39,7 +77,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 return shoppingCart.Product.Price;
             else
             {
-                if(shoppingCart.Count<=100)
+                if (shoppingCart.Count <= 100)
                     return shoppingCart.Product.Price50;
                 else
                     return shoppingCart.Product.Price100;
