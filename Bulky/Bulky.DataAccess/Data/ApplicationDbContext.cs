21636815b5ef31ce858cfaa54f1f278630beebdb
sbasar <senolbasaar@gmail.com>
2024-05-28 22:18:27 +0300

    --OrderHeader ve OrderDetail.cs eklendi.

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCharts { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
