9f0ac789ddb9c2e5e42fe739557b8c2cca9ae300
sbasar <senolbasaar@gmail.com>
2024-06-02 17:54:07 +0300

    ---DBInitializer eklendi.

--- Bulky/BulkyWeb/Program.cs
+++ Bulky/BulkyWeb/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using BulkyBook.Utility;
 using Bulky.Book.Utility;
 using Stripe;
+using BulkyBook.DataAccess.DBInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,10 +40,11 @@ builder.Services.AddSession(options => {
 
 });
 
-
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
+
 
 var app = builder.Build();
 
@@ -64,9 +66,20 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
+SeedDatabase();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer= scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+
+}
